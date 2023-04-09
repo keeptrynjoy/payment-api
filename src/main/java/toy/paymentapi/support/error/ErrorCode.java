fd54d74b88e1db53498634c656131390ebd92224
keeptrynjoy <sungmin4218@gmail.com>
2023-04-09 19:19:13 +0900

    [Test] 재고 수량 검증

--- src/main/java/toy/paymentapi/support/error/ErrorCode.java
+++ src/main/java/toy/paymentapi/support/error/ErrorCode.java
@@ -1,19 +1,21 @@
 package toy.paymentapi.support.error;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 import static org.springframework.http.HttpStatus.*;
 
 @AllArgsConstructor
+@Getter
 public enum ErrorCode {
 
     NOT_ENOUGH_STOCK_QUANTITY(NOT_FOUND,"주문 요청한 상품의 수량보다 재고가 부족합니다."),
     ITEM_STOCK_EMPTY(NOT_FOUND,"주문한 상품의 보유 재고가 없습니다.");
 
     private final HttpStatus httpStatus;
-    private final String message;
+    private final String detail;
 
     /* 404 NOT FOUND */
     public static PaymentApiException throwNotEnoughStockQuantity(){
--- src/test/java/toy/paymentapi/repository/ItemRepositoryTest.java
+++ src/test/java/toy/paymentapi/repository/ItemRepositoryTest.java
@@ -1,17 +1,17 @@
 package toy.paymentapi.repository;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import toy.paymentapi.domain.Item;
+import toy.paymentapi.support.error.ErrorCode;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
-import static toy.paymentapi.domain.QItem.item;
 
 @Slf4j
 @SpringBootTest
@@ -23,6 +23,7 @@ class ItemRepositoryTest {
     @Autowired
     private ItemQueryRepository itemQueryRepository;
 
+    @DisplayName("상품 등록")
     @Test
     void save(){
         //given
@@ -36,8 +37,9 @@ class ItemRepositoryTest {
         assertThat(findItem).isEqualTo(saveItem);
     }
 
+    @DisplayName("재고 수량 검증 - 정상")
     @Test
-    void 재고수량검증(){
+    void findStockSuccess(){
         //given
         Item item = getItem();
         Item saveItem = itemRepository.save(item);
@@ -45,6 +47,23 @@ class ItemRepositoryTest {
         //when&then
         Item findItem = itemQueryRepository.checkStockQuantity(saveItem.getId(), 9);
         assertThat(findItem).isEqualTo(saveItem);
+    }
+
+    @DisplayName("재고 수량 검증 - 재고 부족")
+    @Test
+    void findStockException(){
+        //given
+        Item item = getItem();
+        Item saveItem = itemRepository.save(item);
+
+        //when
+        Item findItem = itemQueryRepository.checkStockQuantity(saveItem.getId(), 20);
+
+        //&then
+        assertThatThrownBy(()->findItem.getId())
+                .isInstanceOf(RuntimeException.class)
+                .extracting("errorCode")
+                .isEqualTo(ErrorCode.NOT_ENOUGH_STOCK_QUANTITY);
     }
 
     private static Item getItem() {
