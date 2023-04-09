17581753b36152353cf48e0e05760045adb7b85e
keeptrynjoy <sungmin4218@gmail.com>
2023-04-09 19:20:29 +0900

    [Feat] custom 예외 세팅

--- src/main/java/toy/paymentapi/support/error/GlobalExceptionHandler.java
+++ src/main/java/toy/paymentapi/support/error/GlobalExceptionHandler.java
@@ -1,5 +1,18 @@
 package toy.paymentapi.support.error;
 
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
 public class GlobalExceptionHandler{
+
+    @ExceptionHandler(value = PaymentApiException.class)
+    protected ResponseEntity<ErrorResponse> PaymentApiException(PaymentApiException ex) {
+        log.error("PaymentApiEException :: ", ex);
+
+        ErrorCode error_code = ex.getErrorCode();
+        return ResponseEntity.status(error_code.getHttpStatus()).body(ErrorResponse.toErrorResponse(error_code));
+    }
 }
