c37607eaa7066eb8f5692b45b15b6270028d81bd
keeptrynjoy <sungmin4218@gmail.com>
2023-04-09 19:20:39 +0900

    [Feat] custom 예외 세팅

---
+++ src/main/java/toy/paymentapi/support/error/ErrorResponse.java
@@ -0,0 +1,34 @@
+package toy.paymentapi.support.error;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+@Getter
+@EqualsAndHashCode
+public class ErrorResponse {
+    private final HttpStatus status;
+    private final String message;
+    private final List<String> errors;
+
+    public ErrorResponse(HttpStatus status, String message, String error) {
+        this.status = status;
+        this.message = message;
+        this.errors = List.of(error);
+    }
+
+    public ErrorResponse(HttpStatus status, String message, List<String> errors) {
+        this.status = status;
+        this.message = message;
+        this.errors = errors;
+    }
+
+    public static ErrorResponse toErrorResponse(ErrorCode errorCode) {
+        return new ErrorResponse(
+                errorCode.getHttpStatus(),
+                errorCode.name(),
+                errorCode.getDetail());
+    }
+}
