a62d9339008550376970c4fd3c7f1ebd04f94605
keeptrynjoy <sungmin4218@gmail.com>
2023-04-09 19:19:55 +0900

    [Test] 재고 수량 증-차감 검증

--- src/main/java/toy/paymentapi/domain/Item.java
+++ src/main/java/toy/paymentapi/domain/Item.java
@@ -4,11 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import toy.paymentapi.support.error.ErrorCode;
+import toy.paymentapi.support.error.PaymentApiException;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 @Entity
 @Getter
@@ -24,6 +28,12 @@ public class Item {
     private LocalDateTime registerDate;
     private int stockQuantity;
 
+    @OneToMany(mappedBy = "item")
+    private List<Coupon> coupons = new ArrayList<>();
+
+    @OneToMany(mappedBy = "item")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
     public Item(String name, int price, LocalDateTime registerDate, int stockQuantity) {
         this.name = name;
         this.price = price;
@@ -31,9 +41,26 @@ public class Item {
         this.stockQuantity = stockQuantity;
     }
 
-    @OneToMany(mappedBy = "item")
-    private List<Coupon> coupons = new ArrayList<>();
+    //== 비즈니스 로직 ==//
+    /**
+    * stock 증가
+    **/
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
 
-    @OneToMany(mappedBy = "item")
-    private List<OrderItem> orderItems = new ArrayList<>();
+
+    /**
+    * stock 감소
+    **/
+    public void removeStock(int quantity){
+
+        int restStock = this.stockQuantity - quantity;
+
+        if (restStock < 0) {
+            throw ErrorCode.throwNotEnoughStockQuantity();
+        }
+
+        this.stockQuantity = restStock;
+    }
 }
---
+++ src/test/java/toy/paymentapi/domain/ItemTest.java
@@ -0,0 +1,52 @@
+package toy.paymentapi.domain;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import toy.paymentapi.repository.ItemRepository;
+import toy.paymentapi.support.error.ErrorCode;
+import toy.paymentapi.support.error.PaymentApiException;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+@Transactional
+class ItemTest {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @DisplayName("재고 차감 - 정상 확인")
+    @Test
+    void removeStockSuccess(){
+        //given
+        Item item = new Item("사과", 10000, LocalDateTime.now(), 10);
+
+        //when
+        item.removeStock(5);
+        Item saveItem = itemRepository.save(item);
+
+        //then
+        Assertions.assertThat(saveItem.getStockQuantity()).isEqualTo(5);
+    }
+
+    @DisplayName("재고 차감 - 예외 발생")
+    @Test
+    void removeStockException(){
+        //given
+        Item item = new Item("사과", 10000, LocalDateTime.now(), 1);
+
+        //when&then
+        Assertions.assertThatThrownBy(()->item.removeStock(5))
+                .isInstanceOf(PaymentApiException.class)
+                .extracting("errorCode")
+                .isEqualTo(ErrorCode.NOT_ENOUGH_STOCK_QUANTITY);
+    }
+}
