582615338a98727b9cc59175fb0a8e9af8c9741d
keeptrynjoy <sungmin4218@gmail.com>
2023-04-09 19:21:02 +0900

    [Feat] custom 예외 세팅

--- src/main/java/toy/paymentapi/support/error/PaymentApiException.java
+++ src/main/java/toy/paymentapi/support/error/PaymentApiException.java
@@ -1,9 +1,12 @@
 package toy.paymentapi.support.error;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class PaymentApiException extends RuntimeException{
 
     private final ErrorCode errorCode;
+
 }
