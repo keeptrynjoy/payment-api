ea396e3781165321a0e45172cd6bb200067a0481
keeptrynjoy <sungmin4218@gmail.com>
2023-04-11 21:54:44 +0900

    [Feat] 주문 상품 조회 및 리스트화 메서드 추가

--- src/main/java/toy/paymentapi/service/OrderService.java
+++ src/main/java/toy/paymentapi/service/OrderService.java
@@ -2,8 +2,34 @@ package toy.paymentapi.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import toy.paymentapi.domain.Item;
+import toy.paymentapi.domain.OrderItem;
+import toy.paymentapi.repository.ItemRepository;
+import toy.paymentapi.service.dto.OrderItemDto;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
+
+    private final ItemRepository itemRepository;
+
+
+    private List<OrderItem> createOrderItems(OrderItemDto... orderItemDtos) {
+
+        List<OrderItem> orderItems = new ArrayList<>();
+
+        for (OrderItemDto or : orderItemDtos) {
+            Item item = itemRepository.getReferenceById(or.getId());
+            OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), or.getCount());
+            orderItems.add(orderItem);
+        }
+
+        return orderItems;
+
+    }
+
+
 }
