033aee8bdc18de99c2ed99b7d545a85a537a16ce
keeptrynjoy <sungmin4218@gmail.com>
2023-04-21 20:39:58 +0900

    [Feat] 커스텀 에러 추가

--- src/main/java/toy/paymentapi/support/error/ErrorCode.java
+++ src/main/java/toy/paymentapi/support/error/ErrorCode.java
@@ -2,7 +2,6 @@ package toy.paymentapi.support.error;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 import static org.springframework.http.HttpStatus.*;
@@ -12,10 +11,29 @@ import static org.springframework.http.HttpStatus.*;
 public enum ErrorCode {
 
     NOT_ENOUGH_STOCK_QUANTITY(BAD_REQUEST,"주문 요청한 상품의 수량보다 재고가 부족합니다."),
-    ITEM_STOCK_EMPTY(BAD_REQUEST,"주문한 상품의 보유 재고가 없습니다."),
-    NOT_ENOUGH_POINT(BAD_REQUEST,"사용할 포인트가 부족합니다.");
+    STOCK_EMPTY(BAD_REQUEST,"주문한 상품의 보유 재고가 없습니다."),
+    NOT_ENOUGH_POINT(BAD_REQUEST,"사용할 포인트가 부족합니다."),
+    NOT_FOUND_ORDER(NOT_FOUND, "주문 내역 확인 불가"),
+    NOT_MATCH_AMOUNTS(BAD_REQUEST,"결제 금액 불일치");
 
     private final HttpStatus httpStatus;
     private final String detail;
 
+
+    public static PaymentApiException throwNotEnoughStockQuantity(){
+        throw new PaymentApiException(NOT_ENOUGH_STOCK_QUANTITY);
+    }
+
+    public static PaymentApiException throwStockEmpty(){
+        throw new PaymentApiException(STOCK_EMPTY);
+    }
+    public static PaymentApiException throwNotEnoughPoint(){
+        throw new PaymentApiException(NOT_ENOUGH_POINT);
+    }
+    public static PaymentApiException throwNotFoundOrder(){
+        throw new PaymentApiException( NOT_FOUND_ORDER);
+    }
+   public static PaymentApiException throwNotMatchAmounts(){
+        throw new PaymentApiException(NOT_MATCH_AMOUNTS);
+    }
 }
