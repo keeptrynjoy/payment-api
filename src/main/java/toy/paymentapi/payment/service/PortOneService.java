e7b4ac916bad9e47836d9cbead505f8889dd060a
keeptrynjoy <sungmin4218@gmail.com>
2023-04-22 21:45:41 +0900

    [Fix] 토큰 발급 - null 반환 해결완료

--- src/main/java/toy/paymentapi/payment/service/PortOneService.java
+++ src/main/java/toy/paymentapi/payment/service/PortOneService.java
@@ -8,14 +8,19 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 import toy.paymentapi.payment.dto.PortOneAccessDto;
+
+import java.util.Map;
 
 import static toy.paymentapi.payment.service.PortOneRestApi.*;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@Transactional
 public class PortOneService {
 
     @Value("${imp_key}")
@@ -24,31 +29,52 @@ public class PortOneService {
     @Value("${imp_secret}")
     private String secret;
 
+    /**
+    * 토큰 발급 요청 처리
+     * return
+     * - PortOneAccessDto
+     *      -  access_token
+    **/
+    @Transactional(propagation = Propagation.MANDATORY)
     public PortOneAccessDto getToken(){
         RestTemplate restTemplate = new RestTemplate();
 
-        //create post-entity
+        //Http Entity create
         HttpEntity<PortOneAccessDto> request = new HttpEntity<>(new PortOneAccessDto(key, secret));
 
-        ResponseEntity<String> parentResponse = restTemplate
-                .exchange(creatApiUrl(AUTHENTICATE.getUrl()), HttpMethod.POST, request, String.class);
+        //POST REQUEST to PortOne API -> Response : JSON
+        ResponseEntity<String> response = restTemplate
+                .exchange(creatApiUrl(AUTHENTICATE), HttpMethod.POST, request, String.class);
 
-        if(!parentResponse.getStatusCode().isError()){
+        if(!response.getStatusCode().isError()){
             Gson gson = new Gson();
 
-            return gson.fromJson(parentResponse.getBody(), PortOneAccessDto.class);
+            //JSON Parser -> convert String
+            String responseBody = gson.fromJson(response.getBody(), Map.class).get("response").toString();
+
+            //JSON Parser -> convert DTO
+            return gson.fromJson(responseBody, PortOneAccessDto.class);
         }
 
         throw new RuntimeException("An access token was not issued from Port One.");
     }
 
+    @Transactional
+    public void getPaymentInfo(){
 
-    private String creatApiUrl(String resource){
+        PortOneAccessDto token = getToken();
+
+        RestTemplate restTemplate = new RestTemplate();
+
+    }
+
+
+    private String creatApiUrl(PortOneRestApi resource){
 
         StringBuilder sb = new StringBuilder();
 
-        sb.append(HOST.getUrl());
-        sb.append(resource);
+        sb.append(HOST.getUri());
+        sb.append(resource.getUri());
 
         String getTokenUrl = sb.toString();
 
