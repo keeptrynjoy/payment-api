5ef6e095acd7d974b6c7c08c8f01a4734bdef5d2
keeptrynjoy <sungmin4218@gmail.com>
2023-04-22 20:48:01 +0900

    [Test] Port One 토큰 발급 테스트 - '정상처리' 완료

---
+++ src/test/java/toy/paymentapi/payment/service/PortOneServiceTest.java
@@ -0,0 +1,28 @@
+package toy.paymentapi.payment.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import toy.paymentapi.payment.dto.PortOneAccessDto;
+
+@SpringBootTest
+@Slf4j
+class PortOneServiceTest {
+
+
+
+    @Autowired
+    PortOneService portOneService;
+
+    @DisplayName("토근 발급 테스트 - 정상 처리")
+    @Test
+    void getTokenTest(){
+        //given
+        //when
+        PortOneAccessDto token = portOneService.getToken();
+
+        //then
+    }
+}
