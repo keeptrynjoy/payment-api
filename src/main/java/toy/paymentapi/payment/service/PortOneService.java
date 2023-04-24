a3f4ad6df66d94e9a62cbc8465ac399dee26316d
keeptrynjoy <sungmin4218@gmail.com>
2023-04-24 16:58:33 +0900

    [Refactor] JSON Parsing 중복 작업 메서드화

--- src/main/java/toy/paymentapi/payment/service/PortOneService.java
+++ src/main/java/toy/paymentapi/payment/service/PortOneService.java
@@ -1,7 +1,9 @@
 package toy.paymentapi.payment.service;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,13 +11,12 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 import toy.paymentapi.payment.dto.PortOneAccessDto;
 
-import java.util.Map;
 
 import static toy.paymentapi.payment.service.PortOneRestApi.*;
 
@@ -32,17 +33,17 @@ public class PortOneService {
     private String secret;
 
     @Transactional
-    public void getPaymentInfo(){
+    public void getPaymentInfo(String impUid){
         RestTemplate restTemplate = new RestTemplate();
 
         //1. 토큰 발급
         PortOneAccessDto token = getToken(restTemplate);
 
         //2. GET 요청
-
+        ResponseEntity<String> response = restTemplate.getForEntity(creatApiUrl(PAYMENTS_IMPUID) +"/"+ impUid, String.class);
 
         //3. JSON Parser -> convert DTO
-
+//        parsingResponseBody(response.getBody(),)
 
         //4. 예외 처리
 
@@ -54,7 +55,7 @@ public class PortOneService {
      * - PortOneAccessDto
      *   - String access_token
     **/
-    @Transactional(propagation = Propagation.MANDATORY)
+//    @Transactional(propagation = Propagation.MANDATORY)
     public PortOneAccessDto getToken(RestTemplate restTemplate){
 
         //Http Entity create
@@ -71,25 +72,30 @@ public class PortOneService {
             throw new RuntimeException("Error Response "+ ex.getMessage());
         }
 
+        return parsingResponseBody(response,PortOneAccessDto.class);
+    }
+
+    private <T> T parsingResponseBody(ResponseEntity<String> response, Class<T> returnClass){
 
         if(!response.getStatusCode().isError()){
-            Gson gson = new Gson();
-            String responseBody ="";
+            ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
 
+            JsonNode responseBody = null;
             try {
                 //JSON Parser -> convert String
-                 responseBody = gson.fromJson(response.getBody(), Map.class).get("response").toString();
-
-            } catch (JsonSyntaxException ex){
+                JsonNode root = mapper.readTree(response.getBody());
+                responseBody = root.path("response");
+            } catch (JsonProcessingException ex){
                 log.error(ex.getMessage());
                 throw new IllegalStateException("JSON Document has no Body");
             }
 
             //JSON Parser -> convert DTO
-            return gson.fromJson(responseBody, PortOneAccessDto.class);
+            return mapper.convertValue(responseBody, returnClass);
         }
 
-        throw new RuntimeException("An access token was not issued from Port One.");
+        throw new ResponseStatusException(response.getStatusCode()
+                ,"received the error code from Port One");
     }
 
 
@@ -104,4 +110,6 @@ public class PortOneService {
 
         return getTokenUrl;
     }
+
+
 }
