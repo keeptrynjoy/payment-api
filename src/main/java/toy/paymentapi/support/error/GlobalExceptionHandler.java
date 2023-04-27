b74e4ee904e76712883745b1df9fe910e067eeb2
keeptrynjoy <sungmin4218@gmail.com>
2023-04-28 05:03:57 +0900

    [Feat] 404, valid exception handler 추가

--- src/main/java/toy/paymentapi/support/error/GlobalExceptionHandler.java
+++ src/main/java/toy/paymentapi/support/error/GlobalExceptionHandler.java
@@ -5,17 +5,36 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    //== 404, valid 검증 예외 발생 ==//
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        log.error("MethodArgumentNotValidException :: ", ex);
+
+        List<String> errors =generateErrors(ex);
+        ErrorResponse response = new ErrorResponse(BAD_REQUEST, ex.getLocalizedMessage(), errors);
+
+        return ResponseEntity.status(response.getStatus()).headers(headers).body(response);
+    }
 
     @Override
     protected ResponseEntity<Object> handleNoHandlerFoundException(
@@ -34,5 +53,15 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
         ErrorCode error_code = ex.getErrorCode();
         return ResponseEntity.status(error_code.getHttpStatus()).body(ErrorResponse.toErrorResponse(error_code));
+    }
+
+    private List<String> generateErrors(BindException ex) {
+        List<String> errors = new ArrayList<>();
+        List<ObjectError> allErrors = ex.getBindingResult().getAllErrors();
+
+        for (ObjectError error : allErrors) {
+            errors.add(error.getDefaultMessage());
+        }
+        return errors;
     }
 }
