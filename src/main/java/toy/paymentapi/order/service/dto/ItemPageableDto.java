a8d5e06d9d7d350287e918a8a1f9c677e83227c9
keeptrynjoy <sungmin4218@gmail.com>
2023-04-28 19:44:30 +0900

    [Feat] Builder Getter 추가

--- src/main/java/toy/paymentapi/order/service/dto/ItemPageableDto.java
+++ src/main/java/toy/paymentapi/order/service/dto/ItemPageableDto.java
@@ -1,13 +1,18 @@
 package toy.paymentapi.order.service.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 
 import java.util.List;
 
+@Getter
 @AllArgsConstructor
+@Builder
 public class ItemPageableDto {
     private Integer totalItems;
     private List<ItemDto> items;
     private Integer totalPages;
-    private Integer currentPage;
+    private Integer currentPageNum;
+    private Integer lastPageNum;
 }
