20b69d29c1eb675c62d4931dbd1da02931b4106d
keeptrynjoy <sungmin4218@gmail.com>
2023-04-29 19:44:02 +0900

    [Test] 예외 발생 Test 완료

--- src/test/java/toy/paymentapi/order/service/ItemServiceTest.java
+++ src/test/java/toy/paymentapi/order/service/ItemServiceTest.java
@@ -2,6 +2,7 @@ package toy.paymentapi.order.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,8 +12,12 @@ import org.springframework.transaction.annotation.Transactional;
 import toy.paymentapi.order.domain.Item;
 import toy.paymentapi.order.repository.ItemRepository;
 import toy.paymentapi.order.service.dto.ItemPageableDto;
+import toy.paymentapi.support.error.ErrorCode;
+import toy.paymentapi.support.error.PaymentApiException;
 
 import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Slf4j
@@ -27,17 +32,21 @@ class ItemServiceTest {
 
     @BeforeEach
     void init(){
-        for(int i=1; i<=25; i++){
-            Item item = Item.createItem("상품_" + i, i * 100, LocalDateTime.now(), 10);
-            itemRepository.save(item);
-        }
 
     }
 
-    @DisplayName("상품 조회 및 페이징 테스트 - 정상처리")
+    @AfterEach
+    void clear(){
+        itemRepository.deleteAll();
+    }
+
+
+
+    @DisplayName("상품 페이징 테스트 - 정상처리")
     @Test
-    void pagedItemSortByRegisteredAtTest(){
+    void pagedItemTestSuccess(){
         //given
+        saveItem();
         Integer pageNum = 0;
         Integer itemSize = 10;
 
@@ -45,9 +54,48 @@ class ItemServiceTest {
         ItemPageableDto itemPageableDto = itemService.pagedItemSortByRegisteredAt(pageNum, itemSize);
 
         //then
-        Assertions.assertThat(itemPageableDto.getLastPageNum()).isEqualTo(2);
-        Assertions.assertThat(itemPageableDto.getTotalItems()).isEqualTo(25);
-        Assertions.assertThat(itemPageableDto.getCurrentPageNum()).isEqualTo(0);
-        Assertions.assertThat(itemPageableDto.getItems().size()).isEqualTo(10);
+        assertThat(itemPageableDto.getLastPageNum()).isEqualTo(2);
+        assertThat(itemPageableDto.getTotalItems()).isEqualTo(25);
+        assertThat(itemPageableDto.getCurrentPageNum()).isEqualTo(0);
+        assertThat(itemPageableDto.getItems().size()).isEqualTo(10);
+    }
+
+
+    @DisplayName("상품 페이징 테스트 - 등록된 상품이 없을시 예외 발생")
+    @Test
+    void pagedItemTestFail(){
+        //given
+        Integer pageNum = 0;
+        Integer itemSize = 10;
+
+        //when
+        //then
+        Assertions.assertThatThrownBy(()-> itemService.pagedItemSortByRegisteredAt(pageNum, itemSize))
+                .isInstanceOf(PaymentApiException.class)
+                .extracting("errorCode")
+                .isEqualTo(ErrorCode.NO_CONTENT_ITEM);
+    }
+
+    @DisplayName("상품 페이징 테스트 - 없는 페이지 요청시 예외 발생")
+    @Test
+    void pagedItemTestFail2(){
+        //given
+        saveItem();
+        Integer pageNum = 4;
+        Integer itemSize = 10;
+
+        //when
+        //then
+        Assertions.assertThatThrownBy(()-> itemService.pagedItemSortByRegisteredAt(pageNum, itemSize))
+                .isInstanceOf(PaymentApiException.class)
+                .extracting("errorCode")
+                .isEqualTo(ErrorCode.NOT_FOUND_PAGE_NUM);
+    }
+
+    private void saveItem() {
+        for(int i=1; i<=25; i++){
+            Item item = Item.createItem("상품_" + i, i * 100, LocalDateTime.now(), 10);
+            itemRepository.save(item);
+        }
     }
 }
