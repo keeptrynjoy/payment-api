8add7eec79009cf3af1e5445704b2db84cdb0845
keeptrynjoy <sungmin4218@gmail.com>
2023-04-29 20:18:09 +0900

    [Feat] 404, NOT_FOUND_PAGE_NUM custom 예외 추가

--- src/main/java/toy/paymentapi/support/error/ErrorCode.java
+++ src/main/java/toy/paymentapi/support/error/ErrorCode.java
@@ -10,11 +10,17 @@ import static org.springframework.http.HttpStatus.*;
 @Getter
 public enum ErrorCode {
 
+    //== 400, Bad Request ==//
     NOT_ENOUGH_STOCK_QUANTITY(BAD_REQUEST,"주문 요청한 상품의 수량보다 재고가 부족합니다."),
     STOCK_EMPTY(BAD_REQUEST,"주문한 상품의 보유 재고가 없습니다."),
     NOT_ENOUGH_POINT(BAD_REQUEST,"사용할 포인트가 부족합니다."),
-    NOT_FOUND_ORDER(NOT_FOUND, "주문 내역 확인 불가"),
     NOT_MATCH_AMOUNTS(BAD_REQUEST,"결제 금액 불일치"),
+
+    NOT_FOUND_PAGE_NUM(BAD_REQUEST,"요청한 페이지 번호를 찾을 수 없습니다."),
+
+    //== 404, Not Found ==//
+    NOT_FOUND_ORDER(NOT_FOUND, "주문 내역 확인 불가"),
+
 
     //== 204, No Content ==//
     NO_CONTENT_ITEM(NO_CONTENT,"등록된 상품이 없습니다.");
@@ -27,7 +33,9 @@ public enum ErrorCode {
     public static PaymentApiException throwNotEnoughStockQuantity(){
         throw new PaymentApiException(NOT_ENOUGH_STOCK_QUANTITY);
     }
-
+    public static PaymentApiException throwNotFoundPageNum(){
+        throw new PaymentApiException(NOT_FOUND_PAGE_NUM);
+    }
     public static PaymentApiException throwStockEmpty(){
         throw new PaymentApiException(STOCK_EMPTY);
     }
