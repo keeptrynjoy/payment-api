a76630f5eb9a775c2243851d0d647e9a63c4e6fb
keeptrynjoy <sungmin4218@gmail.com>
2023-04-29 20:19:31 +0900

    [Refactor] 상품 페이징 반환 객체 변경

--- src/main/java/toy/paymentapi/order/controller/response/PagedItemsResponse.java
+++ src/main/java/toy/paymentapi/order/controller/response/PagedItemsResponse.java
@@ -2,12 +2,10 @@ package toy.paymentapi.order.controller.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import toy.paymentapi.order.service.dto.ItemDto;
-
-import java.util.List;
+import toy.paymentapi.order.service.dto.ItemPageableDto;
 
 @Getter
 @AllArgsConstructor
-public class ItemListResponse {
-    List<ItemDto> itemDtos;
+public class PagedItemsResponse {
+    private ItemPageableDto pagedItems;
 }
